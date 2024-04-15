1fcf146e4b1f26bcd355fd240625ce7a09871d7f
SarithaJohn <sarithajohn2000@gmail.com>
2024-04-15 15:38:49 -0400

    first commit

---
+++ EcommerceWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
