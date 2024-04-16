eb9cca148da21c61076b03cf51f0251b0af1a7c6
SarithaJohn <sarithajohn2000@gmail.com>
2024-04-16 16:16:30 -0400

    three files added new branch

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+	public class Category
+	{
+		[Key]						//DataAnnotations: primary key
+        public int Id { get; set; }
+		[Required]                  //DataAnnotations: Not null
+        public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+	}
+}
+
